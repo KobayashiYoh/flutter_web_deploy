f2742b41d60b2d56b858108520dec187ecdb978c
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-02 23:07:30 +0900

    ボタンタップ時にイベントを送信する機能を追加

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 import 'firebase_options.dart';
 
@@ -9,6 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
@@ -19,6 +21,10 @@ class MyApp extends StatefulWidget {
   static FirebaseAnalyticsObserver observer =
       FirebaseAnalyticsObserver(analytics: analytics);
 
+  // ユーザーIDをランダムに生成する。
+  // 実際は、ユーザーがログインした際に、そのユーザーIDをセットする。
+  static String userId = const Uuid().v4();
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -28,6 +34,7 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     Future(() async {
+      await MyApp.analytics.setUserId(id: MyApp.userId);
       await MyApp.analytics.logAppOpen();
     });
   }
@@ -45,6 +52,7 @@ class _MyAppState extends State<MyApp> {
         title: 'Flutter Demo Home Page',
         analytics: MyApp.analytics,
         observer: MyApp.observer,
+        userId: MyApp.userId,
       ),
     );
   }
@@ -56,11 +64,13 @@ class MyHomePage extends StatefulWidget {
     required this.title,
     required this.analytics,
     required this.observer,
+    required this.userId,
   });
 
   final String title;
   final FirebaseAnalytics analytics;
   final FirebaseAnalyticsObserver observer;
+  final String userId;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -69,10 +79,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
     setState(() {
       _counter++;
     });
+    await widget.analytics.logEvent(
+      name: 'counter_incremented',
+      parameters: {
+        'count': _counter,
+        'user_id': widget.userId,
+      },
+    );
   }
 
   @override
